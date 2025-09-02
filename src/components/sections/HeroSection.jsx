9ede4015449a620eb2f0414ad5ed097bfb2a1486
Simon Olsson <scmolsson@pm.me>
2025-09-02 16:52:23 +0200

    tailwind config

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Label from "../ui/Label";
+import React from 'react';
+import Label from '../ui/Label';
 
 function HeroSection({ content }) {
   return (
-    <section className="bg-neutral-100 w-screen h-screen pt-30 p-3">
+    <section className="h-screen w-screen bg-neutral-100 p-3 pt-30">
       <article className="">
         <Label text={content.hero.subtitle} primary />
         <h1 className="text-3xl"> {content.hero.title} </h1>
-        <p className="text-base mt-2 w-"> {content.hero.desc} </p>
+        <p className="w- mt-2 text-base"> {content.hero.desc} </p>
       </article>
     </section>
   );
