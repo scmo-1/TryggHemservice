943b887573b912da6fe640229c510e4215b8722e
Simon Olsson <scmolsson@pm.me>
2025-09-22 15:59:25 +0200

    add ref in AccordionItem to get height of dev for better animation

--- src/components/ui/Accordion/AccordionItem.jsx
+++ src/components/ui/Accordion/AccordionItem.jsx
@@ -1,17 +1,16 @@
 import * as Lucide from "lucide-react";
 import { Plus, BadgeCheck } from "lucide-react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { motion, AnimatePresence } from "motion/react";
 
 function AccordionItem({ content }) {
   const [open, setOpen] = useState(false);
   const Icon = Lucide[content.icon];
+  const ref = useRef(null);
 
   const container = {
-    hidden: { opacity: 0, height: 0 },
+    hidden: {},
     open: {
-      opacity: 1,
-      height: "auto",
       transition: {
         staggerChildren: 0.1,
         type: "spring",
@@ -24,7 +23,7 @@ function AccordionItem({ content }) {
 
   const animatedChild = {
     hidden: { opacity: 0, y: 20 },
-    open: { opacity: 1, y: 0, transition: { duration: 0.2 } },
+    open: { opacity: 1, y: 0, transition: { duration: 0.3 } },
   };
 
   return (
@@ -37,41 +36,58 @@ function AccordionItem({ content }) {
         <span className="text-xl">{content.title}</span>
         <Plus className="ms-auto rounded-full bg-violet-600 p-1" size={30} />
       </button>
+
       <AnimatePresence initial={false}>
         {open && (
           <motion.div
-            key={content}
-            variants={container}
-            initial="hidden"
-            animate="open"
-            exit="hidden"
-            className="overflow-hidden bg-violet-950 p-1 text-white"
+            key="content"
+            ref={ref}
+            initial={{ height: 0, opacity: 0 }}
+            animate={{
+              height: ref.current ? ref.current.scrollHeight : "auto",
+              opacity: 1,
+            }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
+            className="overflow-hidden bg-violet-950 text-white"
           >
-            <motion.p variants={animatedChild} className="mb-5">
-              {content.desc}
-            </motion.p>
-            {content.subtitle && (
-              <motion.span
-                variants={animatedChild}
-                className="mb-3 flex items-center gap-2 text-lg"
-              >
-                <BadgeCheck size={20} color="#7f22fe" /> {content.subtitle}
-              </motion.span>
-            )}
-            <motion.ul
-              variants={animatedChild}
-              className="flex flex-col items-baseline gap-3 pb-5"
+            <motion.div
+              variants={container}
+              initial="hidden"
+              animate="open"
+              exit="hidden"
+              className="p-3"
             >
-              {content.list.map((item, idx) => (
-                <motion.li
+              {content.desc && (
+                <motion.p variants={animatedChild} className="mb-5">
+                  {content.desc}
+                </motion.p>
+              )}
+
+              {content.subtitle && (
+                <motion.span
                   variants={animatedChild}
-                  key={idx}
-                  className="flex w-full border-b-1 border-violet-800 pb-1"
+                  className="mb-3 flex items-center gap-2 text-lg"
                 >
-                  {item}
-                </motion.li>
-              ))}
-            </motion.ul>
+                  <BadgeCheck size={20} color="#7f22fe" /> {content.subtitle}
+                </motion.span>
+              )}
+
+              <motion.ul
+                variants={container}
+                className="flex flex-col items-baseline gap-3 pb-5"
+              >
+                {content.list.map((item, idx) => (
+                  <motion.li
+                    key={idx}
+                    variants={animatedChild}
+                    className="flex w-full border-b-1 border-violet-800 pb-1"
+                  >
+                    {item}
+                  </motion.li>
+                ))}
+              </motion.ul>
+            </motion.div>
           </motion.div>
         )}
       </AnimatePresence>
