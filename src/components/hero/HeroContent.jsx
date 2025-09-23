e5f06c121da0e1ecaf78302ee869ec7da8e53d4d
Simon Olsson <scmolsson@pm.me>
2025-09-23 10:31:25 +0200

    link hotfix

--- src/components/hero/HeroContent.jsx
+++ src/components/hero/HeroContent.jsx
@@ -11,8 +11,8 @@ function HeroContent({ content }) {
       <h1 className="text-3xl lg:text-6xl"> {content.hero.title} </h1>
       <p className="mt-2 text-base"> {content.hero.desc} </p>
       <div className="mt-8 flex flex-col gap-3 self-start md:flex-row">
-        <AppLink href={"#"}>{content.hero.ctaPrimary}</AppLink>
-        <AppLink href={"#"} variant="outlined">
+        <AppLink href={"#contact"}>{content.hero.ctaPrimary}</AppLink>
+        <AppLink href={"#services"} variant="outlined">
           {content.hero.ctaSecondary}
         </AppLink>
       </div>
